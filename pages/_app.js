9ea450d69757f57bc06f1bcff41eb8145d832354
SmallDevStudio <smalldevstudio@gmail.com>
2024-05-21 21:42:37 +0700

    line Auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,22 @@
-import { useEffect } from "react";
+import Head from "next/head";
+import { useLine } from "@/lib/hook/useLine";
+
 import "@/styles/globals.css";
 
-const liffID = process.env.NEXT_PUBLIC_LIFF_ID;
-
 export default function App({ Component, pageProps }) {
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  useEffect(async() => {
-      const liff = (await import("@line/liff")).default;
-      try {
-        await liff.init({ liffId: liffID });
-      }catch(err){
-        console.log('liff init error', error.message);
-      }
-      if (!liff.isLoggedIn()) {
-        liff.login();
-      }
-  })
-  return <Component {...pageProps} />;
+  const { liffObject, status } = useLine();
+
+  pageProps.liffObject = liffObject;
+  pageProps.status = status;
+
+  return (
+    <>
+      <Head>
+        <title>One Retail</title>
+        <description>One Retail Application</description>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
