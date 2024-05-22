59b073fd8d05d26c8f49cb4c6eea8692c0b6246b
SmallDevStudio <smalldevstudio@gmail.com>
2024-05-22 21:23:30 +0700

    add form

--- pages/users/adduser.jsx
+++ pages/users/adduser.jsx
@@ -1,23 +1,38 @@
 import { useState } from "react";
-import { useForm } from "react-hook-form";
 
 export default function AddUser() {
-    const { register, handleSubmit } = useForm();
     const [data, setData] = useState("");
 
-    const onSubmit = (data) => setData(data);
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+
+        setData({
+            ...data,
+            [name]: value
+        });
+
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        console.log(data);
+    }
+    
 
     console.log(data);
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <input {...register("empid")} placeholder="empid" />
-            <input {...register("name")} placeholder="name" />
-            <input {...register("phone")} placeholder="phone" />
-            <textarea {...register("address")} placeholder="address" />
-            <input type="submit" />
-            <p>{data}</p>
+        <form onSubmit={handleSubmit}>
+
+            <input type="text" name="empid" value={data.event.target} onChange={handleChange} placeholder="รหัสพนักงาน"/>
+            <input type="text" name="name" value={data.event.target} onChange={handleChange} placeholder="ชื่อ-นามสกุล"/>
+            <input type="text" name="phone" value={data.event.target} onChange={handleChange} placeholder="เบอร์โทรศัพท์"/>
+            <textarea type="text" name="address" value={data.event.target} onChange={handleChange} placeholder="ที่อยู่"/>
+
+            <input type="submit" value="Submit" className="btn btn-primary" />
         </form>
+        
     );
 }
 
