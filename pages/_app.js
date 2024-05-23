6b572b07f5830af116cf4e274f7cc355e9e20cbf
SmallDevStudio <smalldevstudio@gmail.com>
2024-05-24 02:18:12 +0700

    line

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
 import { getIronSession } from "iron-session";
 import { sessionOptions } from "@/lib/session";
+import useLine from "@/lib/hook/useLine";
 import "@/styles/globals.css";
 
 function App({ Component, pageProps }) {
+  const { liffObject, status } = useLine();
+
+  pageProps.liffObject = liffObject;
+  pageProps.status = status;
 
   return (
       <Component {...pageProps} />
