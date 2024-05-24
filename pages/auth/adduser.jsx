3becbfc3ae98aaed6b3677c9fd7d39571e3c8b93
SmallDevStudio <smalldevstudio@gmail.com>
2024-05-24 18:55:13 +0700

    required

--- pages/auth/adduser.jsx
+++ pages/auth/adduser.jsx
@@ -134,7 +134,8 @@ export default function AddUser() {
                                 <input 
                                     id="agree" 
                                     type="checkbox"
-                                    className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 `${errors.agree ? border-red-500 focus:ring-red-500 : border-gray-300}`"
+                                    {...register("agree", { required: true })}
+                                    className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 `${errors.agree ? border-red-500 : border-gray-300}`"
                                 />
                                 <label for="agree" className="ml-2 text-sm font-bold text-gray-900 dark:text-white">ฉันยอมรับข้อตกลงและเงื่อนไข</label>
                                 
