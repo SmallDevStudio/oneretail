c0f17c459c20dd368066ec772a396d7aab923faa
SmallDevStudio <smalldevstudio@gmail.com>
2024-05-24 23:01:48 +0700

    firebase admin

--- config/database.config.js
+++ config/database.config.js
@@ -1,26 +1,26 @@
 export const dbConfig = { 
     "provider": {
        "firebase": {
-         apiKey: process.env.FIREBASE_API_KEY, 
-         authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-         databaseURL: process.env.FIREBASE_DATABASE_URL,
-         projectId: process.env.FIREBASE_PROJECT_ID,
-         storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-         messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-         appId: process.env.FIREBASE_APP_ID,
+         apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY, 
+         authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+         databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+         projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+         storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+         messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+         appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
        },
 
        "firebaseadmin": {
-          type: process.env.FIREBASE_ADMIN_TYPE,
-          project_id: process.env.FIREBASE_ADMIN_PROJECT_ID,
-          private_key_id: process.env.FIREBASE_ADMIN_PRIVATE_KEY_ID,
-          private_key: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
-          client_email: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
-          client_id: process.env.FIREBASE_ADMIN_CLIENT_ID,
-          auth_uri: process.env.FIREBASE_ADMIN_AUTH_URI,
-          token_uri: process.env.FIREBASE_ADMIN_TOKEN_URI,
-          auth_provider_x509_cert_url: process.env.FIREBASE_ADMIN_AUTH_PROVIDER_X509_CERT_URL,
-          client_x509_cert_url: process.env.FIREBASE_ADMIN_CLIENT_X509_CERT_URL
+          type: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_TYPE,
+          project_id: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_PROJECT_ID,
+          private_key_id: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_PRIVATE_KEY_ID,
+          private_key: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_PRIVATE_KEY,
+          client_email: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_CLIENT_EMAIL,
+          client_id: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_CLIENT_ID,
+          auth_uri: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_AUTH_URI,
+          token_uri: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_TOKEN_URI,
+          auth_provider_x509_cert_url: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_AUTH_PROVIDER_X509_CERT_URL,
+          client_x509_cert_url: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_CLIENT_X509_CERT_URL
        },
  
        "mongodb": {
