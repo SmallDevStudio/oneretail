2d85fad3c92e372257c60e58a0e45e7b20ba8265
SmallDevStudio <smalldevstudio@gmail.com>
2024-05-24 18:50:25 +0700

    edit page add user

--- pages/auth/adduser.jsx
+++ pages/auth/adduser.jsx
@@ -52,27 +52,28 @@ export default function AddUser() {
                             website
                         </span>
                     </div>
-                    <div>
-                    <div className="relative mb-4">
+                    <div className="block mb-4">
+                    <div className="relative">
                     <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
                         <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 9h3m-3 3h3m-3 3h3m-6 1c-.306-.613-.933-1-1.618-1H7.618c-.685 0-1.312.387-1.618 1M4 5h16a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1Zm7 5a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z"/>
                         </svg>
                     </div>
                     <input 
-                        className="bg-gray-200 border border-gray-300 text-gray-900 text-lg rounded-3xl focus:ring-blue-500 focus:border-blue-500 block w-full ps-12 p-2  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-                        type="number"
+                        className="bg-gray-200 border border-gray-300 text-gray-900 text-lg rounded-3xl `{errors.empid ? focus:ring-red-500 focus:border-red-500 : focus:ring-blue-500 focus:border-blue-500 }` focus:ring-blue-500 focus:border-blue-500 block w-full ps-12 p-2  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 " 
+                        type="text"
                         placeholder="รหัสพนักงาน" 
-                        {...register("empid", { required: true, maxLength: 5, minLength: 5 })}
+                        {...register("empid", { required: true, pattern: /^[0-9]+$/i })}
                     />
                     </div>
-                        {errors.name && errors.name.type === "required" && <span>ช่องนี้จำเป็นต้องกรอกข้อมูล</span>}
-                        {errors.name && errors.name.type === "maxLength" && <span>ข้อความยาวเกินไป</span> }
-                        {errors.name && errors.name.type === "maxLength" && <span>ข้อความน้อยเกินไป</span> }
+                        <div className="text-red-500 text-sm font-bold text-left ml-10 h-2">
+                            {errors.empid && errors.empid.type === "required" && <span>ช่องนี้จำเป็นต้องกรอกข้อมูล</span>}
+                            {errors.empid && errors.empid.type === "pattern" && <span>ตัวเลขเท่านั้น</span> }
+                        </div>
                     </div>
                         
-                    <div>
-                    <div className="relative mb-4">
+                    <div className="block mb-4">
+                    <div className="relative">
                         <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
                             <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 21a9 9 0 1 0 0-18 9 9 0 0 0 0 18Zm0 0a8.949 8.949 0 0 0 4.951-1.488A3.987 3.987 0 0 0 13 16h-2a3.987 3.987 0 0 0-3.951 3.512A8.948 8.948 0 0 0 12 21Zm3-11a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
@@ -84,14 +85,15 @@ export default function AddUser() {
                             placeholder="ชื่อ-นามสกุล" 
                             {...register("fullname", { required: true, pattern: /^[ก-๏\s]+$/i })}
                         />
-                        
                     </div>
-                        {errors.name && errors.name.type === "required" && <span>ช่องนี้จำเป็นต้องกรอกข้อมูล</span>}
-                        {errors.name && errors.name.type === "pattern" && <span>ข้อมูลไม่ถูกต้อง</span> }
+                        <div className="text-red-500 text-sm font-bold text-left ml-10 h-2">
+                            {errors.fullname && errors.fullname.type === "required" && <span>ช่องนี้จำเป็นต้องกรอกข้อมูล</span>}
+                            {errors.fullname && errors.fullname.type === "pattern" && <span>ชื่อต้องเป็นภาษาไทย</span> }
+                        </div>
                     </div>
                        
-                    <div>
-                    <div className="relative mb-4">
+                    <div className="block mb-4">
+                    <div className="relative">
                         <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
                             <svg className=" w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M18.427 14.768 17.2 13.542a1.733 1.733 0 0 0-2.45 0l-.613.613a1.732 1.732 0 0 1-2.45 0l-1.838-1.84a1.735 1.735 0 0 1 0-2.452l.612-.613a1.735 1.735 0 0 0 0-2.452L9.237 5.572a1.6 1.6 0 0 0-2.45 0c-3.223 3.2-1.702 6.896 1.519 10.117 3.22 3.221 6.914 4.745 10.12 1.535a1.601 1.601 0 0 0 0-2.456Z"/>
@@ -104,11 +106,13 @@ export default function AddUser() {
                         {...register("phone", { required: true, maxLength: 10 })}
                     />
                     </div>
-                        {errors.name && errors.name.type === "required" && <span>ช่องนี้จำเป็นต้องกรอกข้อมูล</span>}
-                        {errors.name && errors.name.type === "maxLength" && <span>ข้อความยาวเกินไป</span> }
+                        <div className="text-red-500 text-sm font-bold text-left ml-10 h-2"> 
+                            {errors.phone && errors.phone.type === "required" && <span>ช่องนี้จำเป็นต้องกรอกข้อมูล</span>}
+                            {errors.phone && errors.phone.type === "maxLength" && <span>ข้อความยาวเกินไป</span> }
+                        </div>
                     </div>
                     
-                    <div>
+                    <div className="block mb-4">
                     <div className="mt-10">
                         <label for="address" className="block mb-2 text-lg font-bold text-gray-900 dark:text-white">ที่อยู่สำหรับจัดส่งของรางวัล</label>
                         <textarea 
@@ -119,20 +123,21 @@ export default function AddUser() {
                             {...register("address", { required: true })}>
                         </textarea>
                     </div>
-                        {errors.address && errors.address.type === "required" && <span>ช่องนี้จำเป็นต้องกรอกข้อมูล</span>}
+                        <div className="text-red-500 text-sm font-bold text-left mt-1 ml-10" >
+                            {errors.address && errors.address.type === "required" && <span>ช่องนี้จำเป็นต้องกรอกข้อมูล</span>}
+                        </div>
                     </div>
 
-                    <div className="mt-10">
+                    <div className="mt-5">
                         <div>
                             <div>
                                 <input 
                                     id="agree" 
                                     type="checkbox"
-                                    className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                                    {...register("agree", { required: true })} 
+                                    className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 `${errors.agree ? border-red-500 focus:ring-red-500 : border-gray-300}`"
                                 />
                                 <label for="agree" className="ml-2 text-sm font-bold text-gray-900 dark:text-white">ฉันยอมรับข้อตกลงและเงื่อนไข</label>
-                                {errors.agree && errors.agree.type === "required" && <span>ต้องยอมรับเงื่อนไข</span>}
+                                
                             </div>
                             <div className="block">
                                 <ul className="text-sm font-light text-gray-900 dark:text-gray-400 list-disc ml-10 mb-4 mt-2 text-left">
