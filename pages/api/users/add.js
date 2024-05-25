2f8301fd1c4acfdde7d3ad0e5b6f1e5cd6c18864
SmallDevStudio <smalldevstudio@gmail.com>
2024-05-26 01:54:23 +0700

    update

--- pages/api/users/add.js
+++ pages/api/users/add.js
@@ -5,8 +5,8 @@ export default async function handler(req, res) {
     if (req.method !== "POST") {
         res.status(405).json({ error: "Method not allowed !!!" });
     } else {
-        const { fullname, phone, address, pictureUrl, role, active, employee_id, line_id } = req.body;
-        console.log(fullname, phone, address, pictureUrl, role, active, employee_id, line_id);
+        const { fullname, phone, address, pictureUrl, employee_id, line_id } = req.body;
+        console.log(fullname, phone, address, pictureUrl, employee_id, line_id);
         await connetMongoDB();
         await Users.create({fullname, phone, address, pictureUrl, role: 'superadmin', active: true, employee_id, line_id});
 
