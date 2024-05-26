6c32a9a40d3e5ae5a3fd356b3ec58278fe7060d0
SmallDevStudio <smalldevstudio@gmail.com>
2024-05-26 17:56:57 +0700

    callback

--- pages/auth/callback.jsx
+++ pages/auth/callback.jsx
@@ -1,11 +1,47 @@
 import { useRouter } from "next/router";
+import useSWR from "swr";
+import useLine from "@/lib/hook/useLine";
+
+const fetcher = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+    return data;
+}
 export default function Callback(props) {
     const { status } = props
     const router = useRouter();
+    const { profile } = useLine();
+    const { userId } = profile;
+    const { data, error } = useSWR('/api/users/verify/'+ userId, fetcher);
+
+    if (error) {
+        return <div>failed to load</div>
+    }
+
+    if (!data) {
+        return router.push('/auth/adduser');
+    }
+
+    if (data) {
+        return router.push('/admin');
+    }
 
     if (status === 'inited') {
         return router.push('/auth/adduser');
     } else if (status === 'registered') {
         return router.push('/');
+    }
+
+
+}
+
+export async function getServerSideProps(context) {
+    const { query } = context;
+    const { code } = query;
+
+    return {
+        props: {
+            code
+        }
     }
 }
