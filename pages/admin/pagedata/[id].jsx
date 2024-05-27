ad52c6cec54d5d21d5dbbb73f38957348612a741
SmallDevStudio <smalldevstudio@gmail.com>
2024-05-27 10:23:11 +0700

    getbyid

--- pages/admin/pagedata/[id].jsx
+++ pages/admin/pagedata/[id].jsx
@@ -1,8 +1,42 @@
 import { AdminLayout } from "@/themes"
 import Edit from "@/components/admin/learning/Edit"
+import { useParams } from "next/navigation";
+import { useRouter } from "next/router";
+import Loading from "@/components/Loading";
 
-const EditPage = ({ params }) => {
+const getPageById = async (id) => {
+    try {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}api/pages/${id}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+        const data = response.json;
+        if (!data) {
+            throw new Error('Data not found');
+        }
+        console.log('data:', data);
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
+}
 
+const EditPage = () => {
+    const router = useRouter();
+    const { id } = router.query;
+    console.log(id);
+
+    if (!id) {
+        return <div>Loading...</div>;
+    }
+    const getData = async () => {
+        const data = await getPageById(id);
+        return data;
+    }
+
+    console.log(getData());
     return <Edit />;
 }
 
