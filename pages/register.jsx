a8167ccd64a06dc0912201b17dea96be30fe20f4
SmallDevStudio <smalldevstudio@gmail.com>
2024-06-03 18:54:49 +0700

    data

--- pages/register.jsx
+++ pages/register.jsx
@@ -31,7 +31,7 @@ export default function Register() {
                 }
             }
         }
-    }, []);
+    }, [data, router]);
 
     const onSubmit = async (data) => {
         setLoading(true);
