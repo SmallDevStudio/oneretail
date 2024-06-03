f596f0449674a51f4817310b2686f9c51f714096
SmallDevStudio <smalldevstudio@gmail.com>
2024-06-03 19:49:01 +0700

    home

--- pages/index.js
+++ pages/index.js
@@ -1,59 +1,24 @@
-'use client'
-import React, { useEffect, useState } from "react";
-import { useSession, signOut } from "next-auth/react";
-import { useRouter } from "next/router";
-import Image from "next/image";
-import Loading from "@/components/Loading";
-import CircularProgress from '@mui/material/CircularProgress';
-import Head from "next/head";
-import CheckUser from "@/lib/hook/chckUsers";
-import useSWR from "swr";
+import React from "react";
+import { useState, useEffect } from "react";
 
-export default function Home() {
-    const { data: session} = useSession();
-    const router = useRouter();
-    const userId = session?.user?.id;
-   
+const HomePage = () => {
+    const [user, setUser] = useState(null);
+
     useEffect(() => {
         const fetchUser = async () => {
-            try {
-                const response = await fetch(`/api/users/${userId}`);
-                const data = await response.json();
-                console.log(data);
-            } catch (error) {
-                console.error(error);
-            }
-        }
+            const res = await fetch("/api/users");
+            const data = await res.json();
+            setUser(data);
+        };
         fetchUser();
     }, []);
 
-
-    return (
-        <>
-            <Head>
-                <title>One Retail</title>
-            </Head>
-            <div
-                style={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    height: '100vh',
-                }}
-            >
-                <div className='text-center justify-center items-center'>
-                    <div className='flex mb-10'>
-                        <Image src="/dist/img/logo-one-retail.png" alt="one Retail Logo" width={150} height={150} priority/>
-                    </div>
-                    <CircularProgress />
-                </div>
-            </div>
-        </>
-    )
-}
-
-Home.auth = true;
-
-
-
+    console.log('user',user);
     
+
+
+
+
+    return <div>Home</div>;
+}
+export default HomePage
