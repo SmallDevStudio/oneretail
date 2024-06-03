cf8da4938e840c08e18701e28e1427c84f635ad6
SmallDevStudio <smalldevstudio@gmail.com>
2024-06-03 19:55:36 +0700

    mongo

--- pages/api/users/[userId].js
+++ pages/api/users/[userId].js
@@ -1,7 +1,6 @@
 import connetMongoDB from "@/lib/services/database/mongodb";
 import Users from "@/database/models/users";
 
-
 export default async function handler(req, res) {
     if (req.method === "GET") {
         const { userId } = req.query;
