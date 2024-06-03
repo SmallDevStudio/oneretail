9e54b76bf650345d44523bc3db3ef83889a85bdc
SmallDevStudio <smalldevstudio@gmail.com>
2024-06-03 20:02:48 +0700

    fetch user

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,14 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import useSWR from "swr";
+import { useSession } from "next-auth/react";
 
 const HomePage = () => {
-    const [user, setUser] = useState(null);
+    const { data: session, status } = useSession();
 
+    const userId = session?.user?.id;
     const fetcher = (url) => fetch(url).then((res) => res.json());
-
-    const { data, error } = useSWR("/api/users", fetcher);
+    const { data, error } = useSWR("/api/users"+userId, fetcher);
 
     console.log('user',data);
     
