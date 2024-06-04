c3c012439700a565a7a400966d40d9b100a373d6
SmallDevStudio <smalldevstudio@gmail.com>
2024-06-05 06:02:11 +0700

    main

--- pages/register.jsx
+++ pages/register.jsx
@@ -29,7 +29,7 @@ export default function Register() {
                 if (typeof window !== 'undefined') {
                     localStorage.setItem('user', data);
                     localStorage.setItem('isRegisterd', true);
-                    router.push('/admin');
+                    router.push('/main');
                 }
             }
         }
@@ -77,7 +77,7 @@ export default function Register() {
             if (typeof window !== 'undefined'){
                 localStorage.setItem('isRegisterd', true);
             }
-            router.push('/admin'); // ไปที่หน้าแอปหลักหลังลงทะเบียน
+            router.push('/main'); // ไปที่หน้าแอปหลักหลังลงทะเบียน
             Alert.success('ลงทะเบียนสําเร็จ');
         } catch (error) {
             console.log(error);
