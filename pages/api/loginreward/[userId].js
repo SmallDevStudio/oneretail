403fcbdcaabf0e779cb9680fbbcb4385de3a8741
SmallDevStudio <smalldevstudio@gmail.com>
2024-06-14 14:14:33 +0700

    loginreward

--- pages/api/loginreward/[userId].js
+++ pages/api/loginreward/[userId].js
@@ -1,73 +1,74 @@
-import connetMongoDB from "@/lib/services/database/mongodb";
+import connectMongoDB from "@/lib/services/database/mongodb";
 import LoginReward from "@/database/models/LoginReward";
 import Point from "@/database/models/Point";
 import sendLineMessage from "@/lib/sendLineMessage";
 
 export default async function handler(req, res) {
-    await connetMongoDB();
+  await connectMongoDB();
 
-    const { userId } = req.query;
+  const { userId } = req.query;
 
-    if (!userId) {
-        return res.status(400).json({ error: 'Missing userId' });
+  if (!userId) {
+    return res.status(400).json({ error: 'Missing userId' });
+  }
+
+  if (req.method === 'GET') {
+    try {
+      const existingReward = await LoginReward.findOne({ userId });
+      const today = new Date().toDateString();
+
+      if (existingReward) {
+        const receivedPointsToday = new Date(existingReward.lastLogin).toDateString() === today;
+        return res.status(200).json({ day: existingReward.day, receivedPointsToday });
+      } else {
+        return res.status(200).json({ day: 0, receivedPointsToday: false });
+      }
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: 'Server error' });
     }
+  }
 
-    if (req.method === 'GET') {
-        try {
-        const existingReward = await LoginReward.findOne({ userId });
-        const today = new Date().toDateString();
+  if (req.method === 'POST') {
+    try {
+      const existingReward = await LoginReward.findOne({ userId });
 
-        if (existingReward && new Date(existingReward.lastLogin).toDateString() === today) {
-            return res.status(200).json({ receivedPointsToday: true });
+      let newPoints = 1;
+      let day = 1;
+
+      if (!existingReward) {
+        await LoginReward.create({ userId, day, lastLogin: new Date() });
+        newPoints = Math.floor(Math.random() * 16) + 15;
+      } else {
+        const lastLogin = new Date(existingReward.lastLogin);
+        const today = new Date();
+
+        if (lastLogin.toDateString() === today.toDateString()) {
+          return res.status(400).json({ error: 'Already received points today' });
         }
 
-        return res.status(200).json({ receivedPointsToday: false });
-        } catch (error) {
-        console.error(error);
-        return res.status(500).json({ error: 'Server error' });
-        }
+        day = existingReward.day >= 30 ? 1 : existingReward.day + 1;
+        await LoginReward.updateOne({ userId }, { day, lastLogin: today });
+      }
+      console.log('newPoints:', newPoints);
+
+      await Point.create({
+        userId,
+        description: 'Login Reward',
+        type: 'earn',
+        point: newPoints,
+      });
+
+      // ส่งข้อความไปที่ LINE
+      const message = `คุณได้รับ ${newPoints} คะแนนจากการ Login Reward!`;
+      sendLineMessage(userId, message);
+
+      return res.status(200).json({ message: 'Points added', points: newPoints, day });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: 'Server error' });
     }
-
-    if (req.method === 'POST') {
-        try {
-        const existingReward = await LoginReward.findOne({ userId });
-
-        let newPoints = 1;
-        let day = 1;
-
-        if (!existingReward) {
-            await LoginReward.create({ userId, day, lastLogin: new Date() });
-            newPoints = Math.floor(Math.random() * 16) + 15;
-        } else {
-            const lastLogin = new Date(existingReward.lastLogin);
-            const today = new Date();
-
-            if (lastLogin.toDateString() === today.toDateString()) {
-            return res.status(400).json({ error: 'Already received points today' });
-            }
-
-            day = existingReward.day >= 30 ? 1 : existingReward.day + 1;
-            await LoginReward.updateOne({ userId }, { day, lastLogin: today });
-        }
-        console.log('newPoints:', newPoints);
-
-        await Point.create({
-            userId,
-            description: 'Login Reward',
-            type: 'earn',
-            point: newPoints,
-        });
-
-        // ส่งข้อความไปที่ LINE
-        const message = `คุณได้รับ ${newPoints} คะแนนจากการLogin Reward!`;
-        sendLineMessage(userId, message);
-
-        return res.status(200).json({ message: 'Points added', points: newPoints });
-        } catch (error) {
-        console.error(error);
-        return res.status(500).json({ error: 'Server error' });
-        }
-    } else {
-        return res.status(405).json({ error: 'Method not allowed' });
-    }
+  } else {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
 };
--- pages/loginreward/index.jsx
+++ pages/loginreward/index.jsx
@@ -1,169 +1,161 @@
 "use client";
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import dynamic from "next/dynamic";
 import { useSession } from "next-auth/react";
 import Swal from "sweetalert2";
 
-
-  const fetchPoints = async (userId) => {
-    try {
-      const response = await fetch(`/api/loginreward/${userId}`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        }
-      });
-
-      const data = await response.json();
-
-      if (response.ok) {
-        Swal.fire({
-          title: 'WELCOME LOGIN',
-          text: `คุณได้รับ ${data.points} points`,
-          icon: 'success',
-          confirmButtonText: 'OK',
-        }).then(() => {
-          window.location.href = '/pulsesurvey';
-        });
-      } else {
-        Swal.fire({
-          title: 'Error',
-          text: data.error,
-          icon: 'error',
-          confirmButtonText: 'OK',
-        });
+const fetchPoints = async (userId) => {
+  try {
+    const response = await fetch(`/api/loginreward/${userId}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
       }
-    } catch (error) {
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      Swal.fire({
+        title: 'WELCOME LOGIN',
+        text: `คุณได้รับ ${data.points} points`,
+        icon: 'success',
+        confirmButtonText: 'OK',
+      }).then(() => {
+        window.location.href = '/pulsesurvey';
+      });
+    } else {
       Swal.fire({
         title: 'Error',
-        text: 'An unexpected error occurred',
+        text: data.error,
         icon: 'error',
         confirmButtonText: 'OK',
       });
     }
-  };
+  } catch (error) {
+    Swal.fire({
+      title: 'Error',
+      text: 'An unexpected error occurred',
+      icon: 'error',
+      confirmButtonText: 'OK',
+    });
+  }
+};
 
-  const checkPointStatus = async (userId) => {
-    try {
-      const response = await fetch(`/api/loginreward/${userId}`, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        }
-      });
+const checkPointStatus = async (userId) => {
+  try {
+    const response = await fetch(`/api/loginreward/${userId}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    });
 
-      const data = await response.json();
-      return data.receivedPointsToday;
-    } catch (error) {
-      console.error('Error checking point status:', error);
-      return false;
+    const data = await response.json();
+    if (data.day !== undefined) {
+      return data.day;
+    } else {
+      console.error('Invalid response:', data);
+      return 0;
+    }
+  } catch (error) {
+    console.error('Error checking point status:', error);
+    return 0;
+  }
+};
+
+const Loginreward = () => {
+  const { data: session, status } = useSession();
+  const [loading, setLoading] = useState(false);
+  const [daysLogged, setDaysLogged] = useState(0);
+
+  useEffect(() => {
+    if (status === 'authenticated' && session) {
+      const userId = session.user.id;
+      checkPointStatus(userId).then(setDaysLogged);
+    }
+  }, [session, status]);
+
+  const handleFetchPoints = async () => {
+    if (session && session.user) {
+      setLoading(true);
+      await fetchPoints(session.user.id);
+      setLoading(false);
     }
   };
-  
 
-const loginreward = () => {
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const { data: session, status } = useSession();
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const [loading, setLoading] = useState(false);
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const [receivedPointsToday, setReceivedPointsToday] = useState(false);
+  const handleGoToMain = () => {
+    window.location.href = '/pulsesurvey';
+  };
 
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    useEffect(() => {
-      if (status === 'authenticated' && session) {
-        const userId = session.user.id;
-        checkPointStatus(userId).then(setReceivedPointsToday);
-      }
-    }, [session, status]);
-  
-    const handleFetchPoints = async () => {
-      if (session && session.user) {
-        setLoading(true);
-        await fetchPoints(session.user.id);
-        setLoading(false);
-      }
-    };
-  
-    const handleGoToMain = () => {
-      window.location.href = '/pulsesurvey';
-    };
+  const LineProgressBar = dynamic(() => import("@/components/LineProgressBar"), { ssr: false });
 
-    const loginPercent = receivedPointsToday /30 * 100;
+  const percentage = (daysLogged % 30) * (100 / 30);
 
+  const items = Array.from({ length: 30 }, (_, i) => i + 1);
+  const getImageSrc = (index) => {
+    if (index <= 10) return '/images/loginreward/Asset198.svg';
+    if (index <= 20) return '/images/loginreward/Asset196.svg';
+    return '/images/loginreward/Asset197.svg';
+  };
 
-    const LineProgressBar = dynamic(() => import("@/components/LineProgressBar"), {ssr: false});
-
-    const items = Array.from({ length: 30 }, (_, i) => i + 1);
-    const getImageSrc = (index) => {
-        if (index <= 10) return '/images/loginreward/Asset198.svg';
-        if (index <= 20) return '/images/loginreward/Asset196.svg';
-        return '/images/loginreward/Asset197.svg';
-    };
-    
-
-    return (
-        <main className="flex items-center justify-center bg-[#0056FF]" style={{ minheight: "100vh", width: "100%" }}>
-            <div className="flex items-center text-center justify-center p-4 min-h-[100vh]">
-                <div className="relative bg-white p-2 rounded-xl">
-                    <div className="flex flex-col p-2">
-                        <div className="flex items-center text-center justify-center mt-[20px]">
-                            <span className="text-[35px] font-black text-[#0056FF] dark:text-white">LOGIN</span>
-                            <span className="text-[35px] font-black text-[#F2871F] dark:text-white ml-2">REWARD</span>
-                        </div>
-
-                        <div className="relative mb-8 mt-10">
-                            <LineProgressBar percent={loginPercent} />
-                        </div>
-
-                        <div className="grid grid-cols-5 gap-1 mb-3">
-                        {items.map((item) => (
-                            <div
-                            key={item}
-                            className={`flex-col inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-x-hidden border-2 border-[#C7DFF5] rounded-lg ${item <= receivedPointsToday ? 'bg-[#C7DFF5]/80' : ''}`}
-                            style={{ width: 60, height: 60 }}
-                            id={item.toString()}
-                            >
-                            <Image
-                                src={getImageSrc(item)}
-                                alt={`Reward ${item}`}
-                                width={40}
-                                height={40}
-                                className="relative"
-                            />
-                            <span className="absolute text-white mt-16 mb-1 pz-1 bg-[#C7DFF5] font-bold rounded-lg text-[8px] w-8">
-                                {item}
-                            </span>
-                            </div>
-                        ))}
-                        
-                        </div>
-                        {receivedPointsToday ? (
-                          <button
-                            onClick={handleGoToMain}
-                            className="bg-[#F68B1F] text-white font-bold py-2 px-4 rounded"
-                          >
-                            ไปยังหน้าหลัก
-                          </button>
-                          ) : (
-                          <button
-                            onClick={handleFetchPoints}
-                            disabled={loading}
-                            className="bg-[#F68B1F] text-white font-bold py-2 px-4 rounded"
-                          >
-                            {loading ? 'Loading...' : 'รับคะแนน'}
-                          </button>
-                        )}
-                    </div>
-
-                </div>
+  return (
+    <main className="flex items-center justify-center bg-[#0056FF]" style={{ minHeight: "100vh", width: "100%" }}>
+      <div className="flex items-center text-center justify-center p-4 min-h-[100vh]">
+        <div className="relative bg-white p-2 rounded-xl">
+          <div className="flex flex-col p-2">
+            <div className="flex items-center text-center justify-center mt-[20px]">
+              <span className="text-[35px] font-black text-[#0056FF] dark:text-white">LOGIN</span>
+              <span className="text-[35px] font-black text-[#F2871F] dark:text-white ml-2">REWARD</span>
             </div>
-        </main>
 
-        
-    );
+            <div className="relative mb-8 mt-10">
+              <LineProgressBar percent={percentage} />
+            </div>
+
+            <div className="grid grid-cols-5 gap-1 mb-3">
+              {items.map((item) => (
+                <div
+                  key={item}
+                  className={`flex-col inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-x-hidden border-2 border-[#C7DFF5] rounded-lg ${item <= (daysLogged % 30) ? 'bg-[#C7DFF5]/80' : ''}`}
+                  style={{ width: 60, height: 60 }}
+                  id={item.toString()}
+                >
+                  <Image
+                    src={getImageSrc(item)}
+                    alt={`Reward ${item}`}
+                    width={40}
+                    height={40}
+                    className="relative"
+                  />
+                  <span className="absolute text-white mt-16 mb-1 pz-1 bg-[#C7DFF5] font-bold rounded-lg text-[8px] w-8">
+                    {item}
+                  </span>
+                </div>
+              ))}
+            </div>
+            {daysLogged ? (
+              <button
+                onClick={handleGoToMain}
+                className="bg-[#F68B1F] text-white font-bold py-2 px-4 rounded"
+              >
+                ไปยังหน้าหลัก
+              </button>
+            ) : (
+              <button
+                onClick={handleFetchPoints}
+                disabled={loading}
+                className="bg-[#F68B1F] text-white font-bold py-2 px-4 rounded"
+              >
+                {loading ? 'Loading...' : 'รับคะแนน'}
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
 
-export default loginreward;
+export default Loginreward;
