508670cc606fb0eb7494d4322e71877155c40c79
SmallDevStudio <smalldevstudio@gmail.com>
2024-06-14 10:53:30 +0700

    game

--- pages/games/quiz.jsx
+++ pages/games/quiz.jsx
@@ -10,11 +10,16 @@ export default function QuizGame() {
 
     return (
         <Provider store={store}>
-            <div className="flex flex-col items-center text-center justify-center bg-blue-500 pb-20" style={{ minHeight: "100vh", width: "100%" }}>
+            <div className="flex flex-col items-center text-center justify-center bg-blue-500 pb-20" style={{ 
+                width: "100%"
+                }}>
                 <div className="flex items-center text-center justify-center p-5 bg-white w-full">
                     <span className="text-[35px] font-black text-[#0056FF]">Games</span>
                 </div>
-                <div className="relative flex flex-col p-5 bg-blue-500 w-full">
+                <div className="relative flex flex-col p-5 bg-blue-500 w-full h-full" style={{
+                    backgroundImage: "url('/images/BGgame/Asset14.png')",
+                    backgroundSize: "cover",
+                }}>
                     <div className="relative bg-white rounded-xl shadow-md border-2 p-3 px-2 min-h-[80vh] w-full mb-5">
                         <Quiz userId={userId} />
                     </div>
