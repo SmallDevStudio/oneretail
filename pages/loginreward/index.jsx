228ed89844214cac5e50fc3d0efc638cb725578c
SmallDevStudio <smalldevstudio@gmail.com>
2024-06-17 10:52:51 +0700

    login

--- pages/loginreward/index.jsx
+++ pages/loginreward/index.jsx
@@ -5,7 +5,7 @@ import dynamic from "next/dynamic";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import axios from "axios";
-import useSWR, { mutate } from "swr";
+import useSWR from "swr";
 import Loading from "@/components/Loading";
 import LoginModal from "@/components/LoginModal";
 
@@ -27,12 +27,10 @@ const Loginreward = () => {
   });
 
   useEffect(() => {
-    if(data) {
       if (loginData.receivedPointsToday) {
         router.push("/");
-      }
     }
-  }, [data, loginData, router]);
+  }, [loginData, router]);
 
   const LineProgressBar = dynamic(() => import("@/components/LineProgressBar"), { ssr: false });
 
