0bb13d05dc9a94c21d48f156264137b6731ad7be
SmallDevStudio <smalldevstudio@gmail.com>
2024-06-20 22:53:53 +0700

    403

--- pages/403.js
+++ pages/403.js
@@ -2,7 +2,6 @@ export default function Custom403() {
     return (
       <div>
         <h1>403 - Forbidden</h1>
-        <p>You don't have permission to access this page.</p>
       </div>
     );
   }
