0c316a9fc9f95da615e2c7f7892ad469fa98c5a1
SmallDevStudio <smalldevstudio@gmail.com>
2024-08-01 23:50:09 +0700

    leaderboard

--- components/ClubLeaderBoardModal.jsx
+++ components/ClubLeaderBoardModal.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Modal from 'react-modal';
 import Image from 'next/image';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import HorizontalRuleIcon from '@mui/icons-material/HorizontalRule';
 
 Modal.setAppElement('#__next');
 
@@ -20,6 +23,7 @@ const customStyles = {
 };
 
 const ClubLeaderBoardModal = ({ isOpen, onRequestClose, data }) => {
+    console.log(data);
     return (
         <Modal
             isOpen={isOpen}
@@ -37,20 +41,31 @@ const ClubLeaderBoardModal = ({ isOpen, onRequestClose, data }) => {
             </div>
             <div className="flex flex-col justify-center items-center w-full">
                 <div className='flex justify-center mt-2 mb-2'>
-                    <h2 className="text-2xl font-bold">{data.fullname}</h2>
+                    <h2 className="text-xl font-bold">{data.name}</h2>
                 </div>
                 <Image 
                     src={data.pictureUrl || '/images/avatar-placeholder.png'} 
-                    alt={data.fullname} 
+                    alt={data.name} 
                     width="100" 
                     height="100" 
                     className="rounded-full"
                 />
+            
+                <div>
+                    {data.arrow && <span className="text-sm"><strong>อัพเดทล่าสุด:</strong> {data.arrow}</span>}
+                    {data.arrow < 0 && <ArrowDownwardIcon style={{ color: 'red' }} className='animate-bounce'/>}
+                    {data.arrow === 0 && <HorizontalRuleIcon style={{ color: 'yellow' }}/>}
+                    {data.arrow > 0 && <ArrowUpwardIcon style={{ color: 'green' }} className='animate-bounce'/>}
+                </div>
+
                 <div className="flex flex-col">
-                    <p className="mt-4 text-sm"><strong>Employee ID:</strong> {data.empId}</p>
-                    {data.position && <p className="text-sm"><strong>Position:</strong> {data.position}</p>}
-                    {data.teamGrop && <p className="text-sm"><strong>Team Group:</strong> {data.teamGrop}</p>}
-                    {data.role && <p className="text-sm"><strong>Role:</strong> {data.role}</p>}
+                    <p className="mt-4 text-sm"><strong>รหัสพนักงาน:</strong> {data.empId}</p>
+                    {data.branch && <p className="text-sm"><strong>Branch/Hub:</strong> {data.branch}</p>}
+                    {data.zone && <p className="text-sm"><strong>Zone/Team:</strong> {data.zone}</p>}
+                    {data.region && <p className="text-sm"><strong>Region:</strong> {data.region}</p>}
+                    {data.achieve && <p className="text-sm"><strong>Achieve:</strong> {(data.achieve * 100).toFixed(2)}%</p>}
+                    {data.rating && <p className="text-sm"><strong>Rating:</strong> {data.rating}</p>}
+                    {data.rank && <p className="text-sm"><strong>Rank:</strong> {data.rank}</p>}
                     {data.active !== undefined && <p className="text-sm"><strong>Active:</strong> {data.active ? "Yes" : "No"}</p>}
                 </div>
             </div>
