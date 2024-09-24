8cd397b0297f9d915022c7444e6f5054f0d392a7
SmallDevStudio <smalldevstudio@gmail.com>
2024-09-25 00:15:23 +0700

    survey dashboard

--- pages/survey/index.jsx
+++ pages/survey/index.jsx
@@ -1,62 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import axios from "axios";
+import useSWR from "swr";
 import { useRouter } from "next/router";
 import { AppLayout } from "@/themes";
 import { IoIosArrowBack } from "react-icons/io";
+import CircularProgress from '@mui/material/CircularProgress';
+import Loading from "@/components/Loading";
+
+const fetcher = (url) => fetch(url).then((res) => res.json());
+
 const Survey = () => {
-
     const router = useRouter();
-    
+    const { data: session, status } = useSession();
+    const userId = session?.user?.id;
+
+    const { data: user, error: userError } = useSWR(() => userId ? `/api/users/${userId}` : null, fetcher);
+
+    const managerGroup = [
+        '10483', '11544', '12857', '22590', '80220', '81195', '81200'
+    ];
+
+    useEffect(() => {
+        if (status === "loading" || !user) return;
+
+        if (user.user.role !== "manager" || user.user.role !== "admin") {
+            router.push("/main");
+        }
+
+        if (managerGroup.includes(user.user.empId)) {
+            const teamGrop = user.user.teamGrop;
+            const Group = user.user.group;
+            router.push("/survey/group?teamGrop=" + teamGrop + "&Group=" + Group);
+        } else {
+            router.push("/survey/team");
+            return;
+        }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [router, status, user]);
+
+    if (status === "loading" || !user) return <Loading />;
     return (
-        <div className="flex-1 flex-col p-2 mb-20">
-            <div className="flex flex-row justify-between items-center gap-2 mt-5 w-full">
-                <IoIosArrowBack
-                    className="text-xl inline text-gray-700"
-                    onClick={() => router.back()}
-                    size={25}
-                />
-                <h2 className="text-3xl font-bold text-[#0056FF]">
-                    รายงานอุณหภูมิ <span className="text-[#F2871F]">ความสุข</span>
-                </h2>
-                <div></div>
-            </div>
-
-            <div className="flex flex-col justify-center items-center gap-5 mt-10 w-full">
-                <div className="flex justify-center items-center p-4 bg-[#0056FF] rounded-xl w-4/6">
-                    <span className="font-bold text-white text-3xl"
-                        onClick={() => router.push("/survey/Retail")}
-                    >
-                        BBD
-                    </span>
-                </div>
-
-                <div className="flex justify-center items-center p-4 bg-[#0056FF] rounded-xl w-4/6">
-                    <span className="font-bold text-white text-3xl"
-                        onClick={() => router.push("/survey/AL")}
-                    >
-                        AL
-                    </span>
-                </div>
-
-                <div className="flex justify-center items-center p-4 bg-[#0056FF] rounded-xl w-4/6">
-                    <span className="font-bold text-white text-3xl"
-                        onClick={() => router.push("/survey/PB")}
-                    >
-                        PB
-                    </span>
-                </div>
-
-                <div className="flex justify-center items-center p-4 bg-[#0056FF] rounded-xl w-4/6">
-                    <span className="font-bold text-white text-3xl"
-                        onClick={() => router.push("/survey/TCON")}
-                    >
-                        TCON
-                    </span>
-                </div>
-            </div>
-            
-           
-            
-        </div>
+        <React.Fragment>
+            <Loading />
+        </React.Fragment>
     );
 };
 
