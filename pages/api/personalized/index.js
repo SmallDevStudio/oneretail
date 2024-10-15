1bf1c9c9f08bd244607a28d9de7713b22c7653ca
SmallDevStudio <smalldevstudio@gmail.com>
2024-10-16 00:26:36 +0700

    api

--- pages/api/personalized/index.js
+++ pages/api/personalized/index.js
@@ -1,5 +1,16 @@
 import connetMongoDB from "@/lib/services/database/mongodb";
 import ContentGen from "@/database/models/ContentGen";
+import Content from "@/database/models/Content";
+import Category from "@/database/models/Category";
+import Subcategory from "@/database/models/Subcategory";
+import Group from "@/database/models/Group";
+import SubGroup from "@/database/models/SubGroup";
+
+export const config = {
+    api: {
+        responseLimit: false,
+    },
+}
 
 export default async function handler(req, res) {
     const { method } = req;
@@ -8,7 +19,14 @@ export default async function handler(req, res) {
     switch (method) {
         case "GET":
             try {
-                const content = await ContentGen.find({ active: true }).sort({ createdAt: -1 }).populate('contents');
+                const content = await ContentGen.find({ active: true })
+                .sort({ createdAt: -1 })
+                .populate('contents');
+                
+                if (!content) {
+                    return res.status(200).json({ success: true, data: [] });
+                }
+
                 res.status(200).json({ success: true, data: content });
             } catch (error) {
                 res.status(400).json({ success: false, error: error.message });
