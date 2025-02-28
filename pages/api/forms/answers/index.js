cf25d05f0af10e1171aef26f03b9a46b37a37cca
SmallDevStudio <smalldevstudio@gmail.com>
2025-02-28 12:08:56 +0700

    update

--- pages/api/forms/answers/index.js
+++ pages/api/forms/answers/index.js
@@ -16,6 +16,7 @@ export default async function handler(req, res) {
             } catch (error) {
                 res.status(400).json({ success: false, error: error.message });
             }
+            break;
         case "POST":
             try {
                 const answer = await UseForms.create(req.body);
