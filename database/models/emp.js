f2adaebf09d651bfb89d6c13ddb6a8c2188ba0ac
SmallDevStudio <smalldevstudio@gmail.com>
2025-04-20 11:50:55 +0700

    emp update

--- database/models/emp.js
+++ database/models/emp.js
@@ -1,18 +1,19 @@
 import mongoose, { Schema } from "mongoose";
 
 const empSchema = new Schema({
-    empId: { type: String, required: true },
-    teamGrop: { type: String},
-    sex: { type: String},
-    branch: { type: String},
-    department: { type: String},
-    group: { type: String},
-    chief_th: { type: String},
-    chief_eng: { type: String},
-    position: { type: String},
-    name: { type: String},
+  empId: { type: String, required: true },
+  teamGrop: { type: String },
+  sex: { type: String },
+  branch: { type: String },
+  department: { type: String },
+  group: { type: String },
+  chief_th: { type: String },
+  chief_eng: { type: String },
+  position: { type: String },
+  name: { type: String },
+  group2: { type: String },
 });
 
-const Emp = mongoose.models.emp || mongoose.model('emp', empSchema);
+const Emp = mongoose.models.emp || mongoose.model("emp", empSchema);
 
 export default Emp;
